cea077a362560937ef6b0f5d405a363d1d54c581
tangshuji <597039869@qq.com>
2022-01-29 18:46:11 +0800

    hot-fix first commit

--- src/main/java/com/woniu/gitdemo/Test.java
+++ src/main/java/com/woniu/gitdemo/Test.java
@@ -5,6 +5,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("ok");
+        System.out.println("good");
     }
 
 }
